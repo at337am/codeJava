b6e1551e35fc200fe6d4b1d4385f51f9d2511f07
at337am <lueqit@gmail.com>
2023-09-02 23:34:06 +0800

    0902提交

--- Day19_Extends/src/com/linyz/a02interfacedemo1/Swim.java
+++ Day19_Extends/src/com/linyz/a02interfacedemo1/Swim.java
@@ -1,15 +1,5 @@
 package com.linyz.a02interfacedemo1;
 
-/*
-* 成员变量:
-*   只能是常量,默认修饰符 public static final
-* 构造方法:
-*   没有
-* 成员方法:
-*   只能是抽象方法,默认修饰符 public abstract
-* */
-
-
 public interface Swim {
     public abstract void swim();
 }
